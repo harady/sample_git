f69adf8384d0b3ff6e30784470b008e2824307f4
harady <harady15@gmail.com>
2015-09-13 09:03:39 +0900

    サブツリーの追加スクリプトを修正。

--- init_subtree.sh
+++ init_subtree.sh
@@ -1,2 +1,2 @@
-$ git remote add sample_subtree git https://github.com/harady/sample_subtree.git
-$ git subtree add --prefix sample_subtree sample_subtree develop --squash
+git remote add sample_subtree git https://github.com/harady/sample_subtree.git
+git subtree add --prefix sample_subtree sample_subtree develop --squash
