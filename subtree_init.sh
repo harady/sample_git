23f950578f9c4b4421c78b334a087ba70078d486
harady <harady15@gmail.com>
2015-09-13 09:53:19 +0900

    シェルスクリプト更新。

--- subtree_init.sh
+++ subtree_init.sh
@@ -1,3 +1,3 @@
 git remote add -f sample_subtree https://github.com/harady/sample_subtree.git
-git subtree add --prefix=sample_subtree/ sample_subtree develop --squash
+git subtree add --prefix=sample_subtree/ --squash sample_subtree develop
 read -p "Press [Enter] key to resume."
